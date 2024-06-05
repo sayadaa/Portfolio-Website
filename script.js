f4d3b29bd625b3d19db9b0ccf5aa109947230309
sayada <sayadaaro@gmail.com>
2024-06-05 02:18:48 -0400

    modified nav & footer

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 /* Code for hovering over nav tabs */
 document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('nav a');
+  const currentUrl = window.location.href;
+
+  navLinks.forEach(link => {
+    if (link.href === currentUrl) {
+      link.setAttribute('aria-current', 'page');
+      link.classList.add('bg-blue-700', 'text-white', 'md:bg-transparent', 'md:text-purple-700', 'dark:bg-blue-600', 'md:dark:bg-transparent', 'md:dark:text-blue-500');
+    } else {
+      link.removeAttribute('aria-current');
+      link.classList.remove('bg-blue-700', 'text-white', 'md:bg-transparent', 'md:text-blue-700', 'dark:bg-blue-600', 'md:dark:bg-transparent', 'md:dark:text-blue-500');
+    }
+  });
+});
+
+
+
+/* Old code for hovering over nav tabs - basic*/
+
+/*document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('nav a');
     const currentUrl = window.location.href;
   
@@ -12,5 +31,5 @@ document.addEventListener('DOMContentLoaded', function() {
         link.classList.remove('bg-blue-700', 'text-white', 'md:bg-transparent', 'md:text-blue-700', 'dark:bg-blue-600', 'md:dark:bg-transparent', 'md:dark:text-blue-500');
       }
     });
-  });
+  });*/
   
